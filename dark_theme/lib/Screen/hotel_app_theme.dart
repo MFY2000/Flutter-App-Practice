ae09ee3c8bc0110dca5ab85974c89b67de67aebb
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-03-14 00:26:50 +0500

    MFY auto commit at 13/03/2022 22:24:43

--- dark_theme/lib/Screen/hotel_app_theme.dart
+++ dark_theme/lib/Screen/hotel_app_theme.dart
@@ -69,17 +69,12 @@ class HotelAppTheme {
 class ThemeService {
   final _box = GetStorage();
   final _key = 'isDarkMode';
-  
-  /// Get isDarkMode info from local storage and return ThemeMode
-  ThemeMode get theme => _loadThemeFromBox() ? ThemeMode.dark : ThemeMode.light;
 
-  /// Load isDArkMode from local storage and if it's empty, returns false (that means default theme is light)
+  ThemeMode get theme => _loadThemeFromBox() ? ThemeMode.dark : ThemeMode.light;
   bool _loadThemeFromBox() => _box.read(_key) ?? false;
   
-  /// Save isDarkMode to local storage
   _saveThemeToBox(bool isDarkMode) => _box.write(_key, isDarkMode);
   
-  /// Switch theme and save to local storage
   void switchTheme() {
     Get.changeThemeMode(_loadThemeFromBox() ? ThemeMode.light : ThemeMode.dark);
     _saveThemeToBox(!_loadThemeFromBox());
