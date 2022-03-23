feb77f16c18d203702c27597a2df726918f59cb7
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-03-23 22:56:14 +0500

    MFY auto commit at 23/03/2022 22:56:05

--- google_ads/test/widget_test.dart
+++ google_ads/test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:google_ads/googleAds.dart';
+
 
 import 'package:google_ads/main.dart';
 
