a1a3d617647ce4ea923a3615f41554deabb1ca1e
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-03-25 21:57:37 +0500

    MFY auto commit at 25/03/2022 21:52:13

--- google_ads/lib/Extra/ad_helper.dart
+++ google_ads/lib/Extra/ad_helper.dart
@@ -1,36 +1,36 @@
-import 'dart:io';
+// import 'dart:io';
 
-class AdHelper {
-  static String get bannerAdUnitId {
-    if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
-    } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
-    } else {
-      throw UnsupportedError("unsupported Platform");
-    }
-  }
+// class AdHelper {
+//   static String get bannerAdUnitId {
+//     if (Platform.isAndroid) {
+//       return 'ca-app-pub-3940256099942544/6300978111';
+//     } else if (Platform.isIOS) {
+//       return 'ca-app-pub-3940256099942544/2934735716';
+//     } else {
+//       throw UnsupportedError("unsupported Platform");
+//     }
+//   }
 
-  static String get interstitialAdUnitId {
-    if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/1033173712';
-    } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/4411468910';
-    } else {
-      throw UnsupportedError("unsupported Platform");
-    }
-  }
+//   static String get interstitialAdUnitId {
+//     if (Platform.isAndroid) {
+//       return 'ca-app-pub-3940256099942544/1033173712';
+//     } else if (Platform.isIOS) {
+//       return 'ca-app-pub-3940256099942544/4411468910';
+//     } else {
+//       throw UnsupportedError("unsupported Platform");
+//     }
+//   }
 
-  static String get rewardedAdUnitId {
-    if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/8673189370';
-    } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/7552160883';
-    } else {
-      throw new UnsupportedError('Unsupported platform');
-    }
-  }
-}
+//   static String get rewardedAdUnitId {
+//     if (Platform.isAndroid) {
+//       return 'ca-app-pub-3940256099942544/8673189370';
+//     } else if (Platform.isIOS) {
+//       return 'ca-app-pub-3940256099942544/7552160883';
+//     } else {
+//       throw new UnsupportedError('Unsupported platform');
+//     }
+//   }
+// }
 
 // import 'dart:io';
 
@@ -65,3 +65,38 @@ class AdHelper {
 //     }
 //   }
 // }
+
+import 'dart:io';
+
+class AdHelper {
+
+  static String get bannerAdUnitId {
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-3940256099942544/6300978111';
+    } else if (Platform.isIOS) {
+      return 'ca-app-pub-3940256099942544/2934735716';
+    } else {
+      throw new UnsupportedError('Unsupported platform');
+    }
+  }
+
+  static String get interstitialAdUnitId {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-3940256099942544/1033173712";
+    } else if (Platform.isIOS) {
+      return "ca-app-pub-3940256099942544/4411468910";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+
+  static String get rewardedAdUnitId {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-3940256099942544/5224354917";
+    } else if (Platform.isIOS) {
+      return "ca-app-pub-3940256099942544/1712485313";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+}
