45d21eb1b8be1bbc8dea4c1760b21f2acb22bf87
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-03-25 21:57:16 +0500

    MFY auto commit at 25/03/2022 21:52:13

---
+++ finger_print_login/lib/Config/constant.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+
+Color primaryColor = Color.fromARGB(255, 0, 255, 221);
+Color secondaryColor = Color.fromARGB(255, 0, 247, 255);
+Color fontColor = Color.fromARGB(255, 0, 0, 0);
+
+LinearGradient gradiant = LinearGradient(
+  colors: [
+    primaryColor,
+    secondaryColor
+  ]
+);
